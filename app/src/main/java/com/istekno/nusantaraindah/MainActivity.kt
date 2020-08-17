4f4693a51b033d9fc3a06cd392065243ffca1e35
rizfa-is <67631569+rizfa-is@users.noreply.github.com>
2020-08-17 14:21:25 +0700

    UPDATE !! Application ready for submission(beta v2)
    Note :
    Adding Splash Screen

--- app/src/main/java/com/istekno/nusantaraindah/MainActivity.kt
+++ app/src/main/java/com/istekno/nusantaraindah/MainActivity.kt
@@ -3,13 +3,19 @@ package com.istekno.nusantaraindah
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.os.HandlerCompat.postDelayed
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //making this activity full screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
---
+++ app/src/main/java/com/istekno/nusantaraindah/SplashScreen.kt
@@ -0,0 +1,28 @@
+package com.istekno.nusantaraindah
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //making this activity full screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+        setContentView(R.layout.splash_screen)
+        supportActionBar?.hide()
+
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this,MainActivity::class.java))
+                finish()
+            }, 1500
+        )
+    }
+}
